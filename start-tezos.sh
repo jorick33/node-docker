246a0f9518566dc5112db58fc9b64801ac52fc33
Thomas Boni <thomas@go2scale.com>
2022-06-03 16:35:06 +0200

    fix: restore history mode to "archive"

--- start-tezos.sh
+++ start-tezos.sh
@@ -9,7 +9,8 @@ init_node() {
 		--net-addr="[::]:$netport" \
 		--connections=$connections \
 		--network=$network \
-		--history-mode=full \
+		--history-mode=archive \
+		--force-history-mode-switch \
 		--cors-origin='*' \
 		--cors-header 'Origin, X-Requested-With, Content-Type, Accept, Range'
 	cat /home/tezos/.tezos-node/config.json
