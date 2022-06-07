25c0f3b4c7069faa99b45cb56c426ccf59cf58bb
Thomas Boni <thomas@go2scale.com>
2022-06-07 02:29:34 +0200

    try to use full mode

--- start-tezos.sh
+++ start-tezos.sh
@@ -9,7 +9,7 @@ init_node() {
 		--net-addr="[::]:$netport" \
 		--connections=$connections \
 		--network=$network \
-		--history-mode=archive \
+		--history-mode=full \
 		--cors-origin='*' \
 		--cors-header 'Origin, X-Requested-With, Content-Type, Accept, Range'
 	cat /home/tezos/.tezos-node/config.json
